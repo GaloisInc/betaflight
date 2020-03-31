14608fa5e85fdf9432676b72c1ed8a98332e8cd6
eschul07 <eschulte@pdx.edu>
2020-03-30 17:32:31 -0700

    Update MAIXBIT.mk

--- make/mcu/MAIXBIT.mk
+++ make/mcu/MAIXBIT.mk
@@ -56,28 +56,71 @@ LD_SCRIPT       = $(LINKER_DIR)/riscv_flash_k210_128k.ld
 endif
 
 #Flags
-ARCH_FLAGS      = rv64imafc
+#ARCH_FLAGS unique to K210
+ARCH_FLAGS      = -march=rv64imafc\
+                  -mabi=lp64f\
+
+#Not sure if we need these for RISCV
 #DEVICE_FLAGS    = #-DSTM32F10X_MD
+
+#A mix of K210 flags and BF flags
 LD_FLAGS       :=  \
-                    -nostartfiles\
-                    -static\
-                    -Wl,\
-                    --gc-sections\
-                    -Wl,\
-                    -static\
-                    -Wl,\
-                    --start-group\
-                    -Wl,\
-                    --whole-archive\
-                    -Wl,\
-                    --no-whole-archive\
-                    -Wl,\
-                    --end-group\
-                    -Wl,\
-                    -EL\
-                    -Wl,\
-                    --no-relax\
-                    -T$(LD_SCRIPT)
+               -nostartfiles\
+               -static\
+               -Wl, -gc-sections,-Map,$(TARGET_MAP)\
+               -Wl, -L$(LINKER_DIR) \
+               $(ARCH_FLAGS) \
+               $(LTO_FLAGS) \
+               $(DEBUG_FLAGS) \
+               -Wl, -static\
+               -Wl, --start-group\
+               -Wl, --whole-archive\
+               -Wl, --no-whole-archive\
+               -Wl, --end-group\
+               -Wl, -EL\
+               -Wl, --no-relax\
+               -Wl, --print-memory-usage \
+               -T$(LD_SCRIPT)
+               $(EXTRA_LD_FLAGS)
+
+#CLAGS unique to K210. These get appended to CFLAGS in Makefile.
+CFLAGS     :=  -mcmodel=medany\
+               #-march=rv64imafc\
+               #-fno-common\
+               #-ffunction-sections\
+               #-fdata-sections\
+               -fstrict-volatile-bitfields\
+               -fno-zero-initialized-in-bss\
+               -ffast-math\
+               -fno-math-errno\
+               -fsingle-precision-constant\
+               -Os\
+               -ggdb\
+               #-std=gnu11\
+               -Wno-pointer-to-int-cast\
+               #-Wall\
+               -Werror=all\
+               -Wno-error=unused-function\
+               -Wno-error=unused-but-set-variable\
+               -Wno-error=unused-variable\
+               -Wno-error=deprecated-declarations\
+               #-Wextra\
+               -Werror=frame-larger-than=32768\
+               -Wno-unused-parameter\
+               -Wno-sign-compare\
+               -Wno-error=missing-braces\
+               -Wno-error=return-type\
+               -Wno-error=pointer-sign\
+               -Wno-missing-braces\
+               -Wno-strict-aliasing\
+               -Wno-implicit-fallthrough\
+               -Wno-missing-field-initializers\
+               -Wno-int-to-pointer-cast\
+               -Wno-error=comment\
+               -Wno-error=logical-not-parentheses\
+               -Wno-error=duplicate-decl-specifier\
+               -Wno-error=parentheses\
+               -Wno-old-style-declaration
 
 # STDPERIPH_SRC   := $(filter-out ${EXCLUDES}, $(STDPERIPH_SRC))
                      
